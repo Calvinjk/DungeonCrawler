19a41b2ea0c1e58af7579d79f40c691fdc8e8758
Calvin Kopitz <calvinjk@umich.edu>
2018-12-11 21:24:21 -0500

    Added comments to improve readability

--- DungeonCrawler/Assets/Scripts/DungeonMapGenerator.cs
+++ DungeonCrawler/Assets/Scripts/DungeonMapGenerator.cs
@@ -24,25 +24,30 @@ public class DungeonMapGenerator : MonoBehaviour {
 
 	// Default map generator uses default values for dimensions and height
 	public GameObject[,] GenerateMap(DungeonType layout){
-		GameObject map = new GameObject("Map");
+		// Create an empty parent object for the map
+		GameObject map = new GameObject("Map");	
 		map.transform.position = new Vector3(0f, 0f, 0f);
 
+		// Switch case based on the layout of map desired
 		switch (layout){
 		// Square layouts will have equal dimensions in the X and Z directions
 		case (DungeonType.Square):
 			return GenerateSquareMap (map);
-		default:
+		default:  // If the code reaches this default block, something went wrong and we should show an error while returning an empty map
 			Debug.LogError ("Invalid DungeonType passed into map generator");
 			return new GameObject[0, 0];
 		}
 	}
 
+	// This function will generate a square map given a parent object to put all the pieces under
 	GameObject[,] GenerateSquareMap(GameObject map){
 		GameObject[,] tiles = new GameObject[maxXDimension, maxXDimension];
 
 		// I will use maxXDimension here exclusively due to both dimensions being equal on a square map
 		for (int i = 0; i < maxXDimension; ++i) {
 			for (int j = 0; j < maxXDimension; ++j) {
+
+				// Create a tile and give it a name based on its location
 				GameObject curTile = Instantiate (floorTile, new Vector3(i, 0, j), Quaternion.identity) as GameObject;
 				curTile.name = "(" + i + ", 0, " + j + ")";
 
@@ -53,11 +58,12 @@ public class DungeonMapGenerator : MonoBehaviour {
 					curTile.GetComponent<Renderer>().material = lightTexture;
 				}
 
+				// Add the generated tile to the tiles array and set its parent to the passed-in parameter.
 				tiles[i, j] = curTile;
 				curTile.transform.SetParent(map.transform);
 			}
 		} 
-
+			
 		return tiles;
 	}
 }
