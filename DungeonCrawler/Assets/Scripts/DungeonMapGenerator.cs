5a1dc60d690e464b57d95e094c17070a1cc9d5ac
Calvin Kopitz <calvinjk@umich.edu>
2019-01-02 23:46:05 -0500

    Successfully generate the first room!  Fixes #51

--- DungeonCrawler/Assets/Scripts/DungeonMapGenerator.cs
+++ DungeonCrawler/Assets/Scripts/DungeonMapGenerator.cs
@@ -10,15 +10,20 @@ public class DungeonMapGenerator : MonoBehaviour {
 	[Tooltip("Toggles debug logs for map generation")]
 	public bool generationDebugLogs = true;
 
-	[Tooltip("Smallest single radius (from door) a room can be")]
-	public int minRoomRadius = 2;
-	[Tooltip("Largest single dimension (from door) a room can be")]
-	public int maxRoomDiameter = 10;
+	[Tooltip("Smallest dimension a room can have.  Lower than 3 can generate a room that has no walkable space")]
+	public int minRoomDiameter = 3;
+	[Tooltip("Largest dimension a room can have")]
+	public int maxRoomDiameter = 20;
+
+	[Tooltip("Number of failed attempts to place a major feature until generator believes it is done")]
+	public int maxAttempts = 100;
 
 	public bool _____________________;
 
 	public const int XDIMDEFAULT = 100;  // Default size of the dungeon floor in the X direction
 	public const int YDIMDEFAULT = 100;  // Default size of the dungeon floor in the Z direction
+
+	public int curRoomAttempts = 0;
 
 	Tile[,] tiles;
 
@@ -65,9 +70,11 @@ public class DungeonMapGenerator : MonoBehaviour {
 		mapScript.tileMap = tiles;
 
 		// Create the initial room in the center (ish)
-		int xLength = Random.Range(minRoomRadius, maxRoomDiameter + 1);
-		int yLength = Random.Range (minRoomRadius, maxRoomDiameter + 1);
+		int xLength = Random.Range(minRoomDiameter, maxRoomDiameter + 1) / 2;
+		int yLength = Random.Range (minRoomDiameter, maxRoomDiameter + 1);
 		GenerateRoom((xSize / 2) - (xLength / 2), (ySize / 2) - (yLength / 2), xLength, yLength, Direction.North);
+
+		// TODO - Keep generating stuff!
 
 		return mapScript;
 	}
@@ -79,18 +86,47 @@ public class DungeonMapGenerator : MonoBehaviour {
 	void GenerateRoom(int xLocation, int yLocation, int xLength, int yLength, Direction direction){
 		if (generationDebugLogs){ 
 			Debug.Log ("Attempting to create room at (" + xLocation + "," + yLocation + ")...\n" +
-			"Dimensions of " + xLength + "x" + yLength + "...\n" +
+				"Dimensions of " + (xLength * 2 + 1) + "x" + yLength + "...\n" +
 				"Direction: " + direction);
 		}
 
 		// Check if we can fit a room here
-		if (CheckTiles(xLocation, yLocation, xLength, yLength, direction)){
-			
+		if (CheckTiles (xLocation, yLocation, xLength, yLength, direction)) {
+			// Good, lets generate it!
+			for (int i = -xLength; i <= xLength; ++i) {
+				for (int j = 0; j < yLength; ++j) {
+					Tile curTile;
+
+					switch (direction) {
+					case Direction.North:
+						curTile = tiles [xLocation + i, yLocation + j];
+						if (Mathf.Abs (i) == xLength || j == 0 || j == (yLength - 1)) {
+							curTile.curTileState = Tile.TileState.Wall;
+							curTile.gameObject.GetComponent<Renderer> ().material.color = Color.grey;
+						} else {
+							curTile.curTileState = Tile.TileState.Open;
+							curTile.gameObject.GetComponent<Renderer> ().material.color = Color.white;
+						}
+						break;
+					case Direction.South:
+
+						break;
+					case Direction.East:
+
+						break;
+					case Direction.West:
+
+						break;
+					}
+				}
+			}
+		} else {
+			++curRoomAttempts;
 		}
 	}
 
 	bool CheckTiles(int xLocation, int yLocation, int xLength, int yLength, Direction direction){
-		if (generationDebugLogs) { Debug.Log ("Checking room..."); }
+		if (generationDebugLogs) { Debug.Log ("Checking attempted room placement..."); }
 
 		/*
 		This code will split for each of the 4 directions.  Therefore, in order to not duplicate
@@ -107,9 +143,9 @@ public class DungeonMapGenerator : MonoBehaviour {
 				switch (direction) {
 				case Direction.North:
 					// Check if this tile is ungenerated
-					if (tiles[xLocation + xLength, yLocation + yLength].curTileState != Tile.TileState.Ungenerated){
+					if (tiles[xLocation + i, yLocation + j].curTileState != Tile.TileState.Ungenerated){
 						// If this tile as already been generated, check if it is a wall
-						if (tiles [xLocation + xLength, yLocation + yLength].curTileState != Tile.TileState.Wall) {
+						if (tiles [xLocation +i, yLocation + j].curTileState != Tile.TileState.Wall) {
 
 							// If the tile is no a wall, we have overlap and should return false
 							if (generationDebugLogs) {
@@ -131,8 +167,8 @@ public class DungeonMapGenerator : MonoBehaviour {
 					}
 					break;
 				case Direction.South:
-					if (tiles [xLocation + xLength, yLocation - yLength].curTileState != Tile.TileState.Ungenerated) {
-						if (tiles [xLocation + xLength, yLocation - yLength].curTileState != Tile.TileState.Wall) {
+					if (tiles [xLocation + i, yLocation - j].curTileState != Tile.TileState.Ungenerated) {
+						if (tiles [xLocation + i, yLocation - j].curTileState != Tile.TileState.Wall) {
 							if (generationDebugLogs) {
 								Debug.Log ("Result: False");
 							}
@@ -149,8 +185,8 @@ public class DungeonMapGenerator : MonoBehaviour {
 					}
 					break;
 				case Direction.East:
-					if (tiles [yLocation + yLength, xLocation + xLength].curTileState != Tile.TileState.Ungenerated) {
-						if (tiles [yLocation + yLength, xLocation + xLength].curTileState != Tile.TileState.Wall) {
+					if (tiles [yLocation + j, xLocation + i].curTileState != Tile.TileState.Ungenerated) {
+						if (tiles [yLocation + j, xLocation + i].curTileState != Tile.TileState.Wall) {
 							if (generationDebugLogs) {
 								Debug.Log ("Result: False");
 							}	
@@ -168,8 +204,8 @@ public class DungeonMapGenerator : MonoBehaviour {
 					break;
 
 				case Direction.West:
-					if (tiles [yLocation - yLength, xLocation + xLength].curTileState != Tile.TileState.Ungenerated) {
-						if (tiles [yLocation - yLength, xLocation + xLength].curTileState != Tile.TileState.Wall) {
+					if (tiles [yLocation - j, xLocation + i].curTileState != Tile.TileState.Ungenerated) {
+						if (tiles [yLocation - j, xLocation + i].curTileState != Tile.TileState.Wall) {
 							if (generationDebugLogs) {
 								Debug.Log ("Result: False");
 							}
